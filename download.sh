868b6ce4d57b2b8785509b9a792beecd52042c4b
Adam Hunter <adamhunter@me.com>
2011-04-14 21:44:40 -0400

    going directly to home when installed

--- download.sh
+++ download.sh
@@ -1,7 +1,6 @@
 echo
 echo "Begining Install..."
 echo "Downloading Rekon"
-echo
 curl -L -s https://github.com/adamhunter/rekon/tarball/master > /tmp/rekon.tar.gz
 echo
 echo "Download Completed"
@@ -16,4 +15,4 @@ cd /tmp/rekon
 cd -
 echo
 echo "Install Completed!"
-open "http://127.0.0.1:8098/riak/rekon/go"
+open "http://127.0.0.1:8098/riak/rekon/go#/buckets"
